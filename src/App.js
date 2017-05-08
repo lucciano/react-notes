ad92a630dceb44b31227b07a85fd805be658854e
Luciano Andrade <andrade.luciano@gmail.com>
2017-05-08 06:38:48 -0300

    ....

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Notes from './app/components/Notes';
 
-
 class App extends Component {
+  constructor(props) {
+	  super(props);
+	  this.state = { notes: [
+	{
+		id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
+		task: 'Learn React'
+	},
+	{
+		id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
+		task: 'Do laundry'
+	}
+	  ]
+	  };
+  }
+
   render() {
+    const {notes} = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-	<Notes />
+      <button onClick={() => console.log('add note')}>+</button>
+	<Notes notes={notes}/>
       </div>
     );
   }
